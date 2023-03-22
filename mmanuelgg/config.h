ce0a461fdf44872ebe16614c63e051683988290f
mmanuelgg <mmanuelgg@gmail.com>
2023-03-22 20:47:36 +0100

    first kyria config

---
+++ mmanuelgg/config.h
@@ -0,0 +1,41 @@
+/* Copyright 2022 Thomas Baart <thomas@splitkb.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#ifdef RGBLIGHT_ENABLE
+// Default
+#    define RGBLIGHT_EFFECT_BREATHING
+#    define RGBLIGHT_HUE_STEP 8
+#    define RGBLIGHT_SAT_STEP 8
+#    define RGBLIGHT_VAL_STEP 8
+#    ifndef RGBLIGHT_LIMIT_VAL
+#    	define RGBLIGHT_LIMIT_VAL 150
+#    endif
+// New
+#    define RGBLIGHT_SLEEP
+#    define RGBLIGHT_TIMEOUT 70000
+#    define RGBLIGHT_SPLIT
+#    define RGBLIGHT_DEFAULT_MODE RGBLIGHT_MODE_BREATHING
+#    define RGBLIGHT_DEFAULT_HUE 250
+//#    define RGBLIGHT_LAYERS
+#endif
+
+// Lets you roll mod-tap keys
+#define IGNORE_MOD_TAP_INTERRUPT
+
+// Tiempo entre pulsaciones para tap dance
+#define TAPPING_TERM 170
---
+++ mmanuelgg/rules.mk
@@ -0,0 +1,11 @@
+CONVERT_TO = elite_pi
+OLED_ENABLE = yes
+OLED_DRIVER = SSD1306      # Enables the use of OLED displays
+ENCODER_ENABLE = yes       # Enables the use of one or more encoders
+RGB_MATRIX_ENABLE = no     # Disable keyboard RGB matrix, as it is enabled by default on rev3
+RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
+SPLIT_KEYBOARD = yes
+WPM_ENABLE = yes
+BOOTMAGIC_ENABLE = yes
+TAP_DANCE_ENABLE = yes
+
