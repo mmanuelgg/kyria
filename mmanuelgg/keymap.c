54d303b51a2988e060c812b541a23d5ff3bd12d5
mmanuelgg <mmanuelgg@gmail.com>
2023-03-31 19:11:14 +0200

    changed knob pgdownup for wheel

--- mmanuelgg/keymap.c
+++ mmanuelgg/keymap.c
@@ -456,9 +456,11 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
     } else if (index == 1) {
         // Page up/Page down
         if (clockwise) {
-            tap_code(KC_PGDN);
+            //tap_code(KC_PGDN);
+            tap_code(KC_WH_U);
         } else {
-            tap_code(KC_PGUP);
+            //tap_code(KC_PGUP);
+            tap_code(KC_WH_D);
         }
     }
     return false;
