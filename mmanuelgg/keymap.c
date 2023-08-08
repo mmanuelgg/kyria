aaa1b716dee934318b67139bc486b069404739e5
Manuel Gonzalez <manuel.gonzalez@tupl.com>
2023-08-08 13:46:43 +0200

    enter space changed

--- mmanuelgg/keymap.c
+++ mmanuelgg/keymap.c
@@ -27,7 +27,7 @@ void leader_end_user(void){
 char wpm_str[10];
 static uint16_t key_timer; // timer para controlar el tiempo
 static void refresh_rgb(void); // revisa si ha habido algún tipo de actividad
-static void check_rgb_timeout(void); // revisa si no ha habido actividad en el tiempo fijado para suspender RGB 
+static void check_rgb_timeout(void); // revisa si no ha habido actividad en el tiempo fijado para suspender RGB
 bool is_rgb_timeout = false; // almacena si el RBG está o no suspendido
 
 void refresh_rgb(){
@@ -142,7 +142,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_TAB  , KC_Q ,  KC_W   ,  KC_E  ,   KC_R ,   KC_T ,                                        KC_Y,   KC_U ,  KC_I ,   KC_O ,  KC_P , KC_BSPC,
      CTL_ESC , KC_A ,  KC_S   ,  KC_D  ,   KC_F ,   KC_G ,                                        KC_H,   KC_J ,  KC_K ,   KC_L , TD(TD_ENE), CTL_QUOT,
      KC_LSFT , KC_Z ,  KC_X   ,  KC_C  ,   KC_V ,   KC_B , KC_LBRC,KC_MUTE,     FKEYS  , KC_RBRC, KC_N,   KC_M ,KC_COMM, KC_DOT ,KC_SLSH, KC_RSFT,
-                                ADJUST , KC_LGUI, ALT_ENT, KC_SPC , NAV   ,     SYM    , KC_SPC , ALG_ENT , NAV , QK_LEAD
+                                ADJUST , KC_LGUI,ALT_T(KC_SPC),KC_ENT,NAV ,     SYM    , KC_SPC , ALG_ENT , NAV , QK_LEAD
     ),
 
 /*
@@ -163,7 +163,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_TAB  ,KC_QUOTE,KC_COMM,  KC_DOT,   KC_P ,   KC_Y ,                                        KC_F,   KC_G ,  KC_C ,   KC_R ,  KC_L , KC_BSPC,
      CTL_ESC , KC_A ,  KC_O   ,  KC_E  ,   KC_U ,   KC_I ,                                        KC_D,   KC_H ,  KC_T ,   KC_N ,  KC_S , CTL_MINS,
      KC_LSFT ,KC_SCLN, KC_Q   ,  KC_J  ,   KC_K ,   KC_X , KC_LBRC,KC_MUTE,     FKEYS  , KC_RBRC, KC_B,   KC_M ,  KC_W ,   KC_V ,  KC_Z , KC_RSFT,
-                                 ADJUST, KC_LGUI, ALT_ENT, KC_SPC , NAV   ,     SYM    , KC_SPC ,ALG_ENT, NAV  , QK_LEAD
+                                 ADJUST, KC_LGUI,ALT_T(KC_SPC),KC_ENT, NAV,     SYM    , KC_SPC ,ALG_ENT, NAV  , QK_LEAD
     ),
 
 /*
